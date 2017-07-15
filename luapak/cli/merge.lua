530a122a6e9a5e761250949986b43df6c7fc7ccb
Jakub Jirutka <jakub@jirutka.cz>
2017-07-16 01:11:32 +0200

    Add command "merge"

---
+++ luapak/cli/merge.lua
@@ -0,0 +1,60 @@
+---------
+-- CLI for the merger module.
+----
+local fs = require 'luapak.fs'
+local optparse = require 'luapak.optparse'
+local utils = require 'luapak.utils'
+local merger = require 'luapak.merger'
+
+local read_file = fs.read_file
+local split = utils.split
+local unpack = table.unpack
+
+
+local help_msg = [[
+.
+Usage: ${PROG_NAME} merge [options] MODULE [MODULE...]
+       ${PROG_NAME} merge [-h | --help]
+
+Arguments:
+  MODULE                    Name and path of Lua module delimited with "="
+                            (e.g. "luapak.utils=luapak/utils.lua") or just path of module.
+
+Options:
+  -g, --debug               Preserve module names and line numbers in error backtraces?
+  -o, --output=FILE         Where to write the generated code. Use "-" for stdout. Default is "-".
+  -v, --verbose             Be verbose, i.e. print debug messages.
+  -h, --help                Display this help message and exit.
+]]
+
+
+--- Runs the merge command.
+--
+-- @function __call
+-- @tparam table arg List of CLI arguments.
+-- @raise if some error occured.
+return function (arg)
+  local optparser = optparse(help_msg)
+  local args, opts = optparser:parse(arg, { output = '-' })
+
+  local modules = {}
+  for _, item in ipairs(args) do
+    local name, path = unpack(split('%=', item))
+    if not path then
+      path = name
+      name = name:gsub('[/\\]', '.'):gsub('^%.+', ''):gsub('.lua$', '')
+    end
+    modules[name] = assert(read_file(path))
+  end
+
+  local out = opts.output == '-'
+      and io.stdout
+      or assert(io.open(opts.output, 'w'))
+
+  merger.merge_modules(modules, opts.debug, function (...)
+      assert(out:write(...))
+    end)
+  assert(out:flush())
+
+  out:close()
+end
