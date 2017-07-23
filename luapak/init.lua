92e7c7213ac98a335bd9d7a12036eb4918e8e98e
Jakub Jirutka <jakub@jirutka.cz>
2017-07-24 01:04:40 +0200

    Release v0.1.0.beta5

--- luapak/init.lua
+++ luapak/init.lua
@@ -6,7 +6,7 @@ return {
   --- The module's name.
   _NAME = 'luapak',
   --- The module's version number.
-  _VERSION = '0.1.0.beta4',
+  _VERSION = '0.1.0.beta5',
   --- The module's homepage.
   _HOMEPAGE = 'https://github.com/jirutka/luapak',
 }
